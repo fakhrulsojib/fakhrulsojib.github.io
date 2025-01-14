82c673de948d6d46233e8bc8d6b6f1c799c0108a
Md Fakhrul Islam (Sojib) <fakhrulsojib@gmail.com>
2025-01-14 23:35:35 +0600

    Remember theme in localstorage added

--- src/hooks/ThemeToggler.tsx
+++ src/hooks/ThemeToggler.tsx
@@ -1,32 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../styles/ThemeToggler.css";
+import { useTheme } from "./useTheme";
 
 const ThemeToggler: React.FC = () => {
-  const [theme, setTheme] = useState<"light" | "dark">("light");
-
-  useEffect(() => {
-    const prefersDark = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    ).matches;
-    const initialTheme = prefersDark ? "dark" : "light";
-    setTheme(initialTheme);
-    document.documentElement.setAttribute("data-theme", initialTheme);
-
-    // Remove any existing theme classes first
-    document.body.classList.remove("light", "dark");
-    // Add the initial theme class
-    document.body.classList.add(initialTheme);
-  }, []);
-
-  const toggleTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme);
-    document.documentElement.setAttribute("data-theme", newTheme);
-
-    // Toggle theme classes
-    document.body.classList.remove(theme);
-    document.body.classList.add(newTheme);
-  };
+  const { theme, toggleTheme } = useTheme();
 
   return (
     <div onClick={toggleTheme} className={`tdnn ${theme}`}>
